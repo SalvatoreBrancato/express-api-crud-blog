de942ce567e059d296eef2cb048eb105a709591a
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-11-14 18:14:18 +0100

    destroy (esercizio completato)

--- controller/postsController.js
+++ controller/postsController.js
@@ -29,8 +29,28 @@ function store(req, res){
     })
 }
 
-function destory(req, res){
+function destroy(req, res){
+    const postIndex = db.findIndex((post)=> post.slug == req.params.slug);
+    //console.log(postIndex)
+    res.format({
+        html:()=>{
+            res.redirect(303, '/')
+        },
+        default:()=>{
+            if(postIndex != -1){
 
+                db.splice(postIndex, 1);
+            
+               
+                fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify(db))
+            
+                res.send('eliminato')
+            }else{
+                res.status(404).send('Post non trovato')
+            }
+        }
+    })
+    
 }
 
 
